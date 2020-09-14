654cc24c114c65082ede993246ef4f84df83d1ec
mandoo <codingmandoo@gmail.com>
2020-09-14 17:07:26 +0900

    실습2) VariableTest.java

---
+++ TodaysFortuneGame/src/VariableTest.java
@@ -0,0 +1,17 @@
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		char myChar='\u0041'; //A
+		System.out.println("myChar = "+ myChar);
+		
+		int myInt=0x15; //21
+		System.out.println("myInt = "+ myInt);
+		
+		float myFloat=15.1F;
+		System.out.println("myFloat = "+ myFloat);
+		
+		System.out.println("Single Quote(\')\tBackslash(\\)");
+	}
+
+}
