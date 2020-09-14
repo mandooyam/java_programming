20bb52484b52b376b331e8e9b3e6dc47cb706c84
mandoo <codingmandoo@gmail.com>
2020-09-14 17:02:40 +0900

    1. 오늘의 운세 게임 실습1) DataTypeTest.java

---
+++ TodaysFortuneGame/src/DataTypeTest.java
@@ -0,0 +1,24 @@
+public class DataTypeTest {
+	public static void main(String[] args) {
+		int i;
+		float f;
+		double d;
+		char c;
+		boolean b;
+		
+		i = 120;
+		f = 12.23f;
+		d = 12.23;
+		c = 'a';
+		b = true;
+		
+		System.out.println("Data Practice Program");
+		System.out.println("---------------------");
+		System.out.println("int : "+i);
+		System.out.println("float : "+f);
+		System.out.println("double : "+d);
+		System.out.println("char : "+c);
+		System.out.println("boolean : "+b);
+	}
+
+}
