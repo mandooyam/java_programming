0fec7ddc6c866321d1c83917dc9115120ccee991
mandoo <codingmandoo@gmail.com>
2020-09-14 17:52:57 +0900

    실습4) IfTest, 실습5) SwitchTest

---
+++ RockPaperScissors/src/IfTest.java
@@ -0,0 +1,14 @@
+
+public class IfTest {
+
+	public static void main(String[] args) {
+		boolean condition = true;
+		
+		if(condition) {
+			System.out.println("조건은 참!");
+		}else{
+			System.out.println("조건은 거짓!");
+		}
+	}
+
+}
