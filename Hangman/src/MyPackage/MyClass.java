f8fddcaec512f2b3900090f7b178aa97255680c9
mandooyam <codingmandoo@gmail.com>
2020-09-16 20:48:59 +0900

    실습2) ConstructorTest, 실습3) MyClass, PackageTest

---
+++ Hangman/src/MyPackage/MyClass.java
@@ -0,0 +1,7 @@
+package MyPackage; // 패키지 선언
+
+public class MyClass {
+	public void myMethod(){
+		System.out.println("자바2 프로그래밍");
+	}
+}
---
+++ Hangman/src/PackageTest.java
@@ -0,0 +1,10 @@
+import MyPackage.*; // 패키지 사용을 선언
+
+public class PackageTest {
+
+	public static void main(String[] args) {
+		MyClass mc = new MyClass(); // MyPackage 내에 있는 MyClass를 사용
+		mc.myMethod(); // 자바2 프로그래밍
+	}
+
+}
